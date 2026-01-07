5693a467b59a62ea68870650a65395ef11ec3d8e
Mahendra shinde <MahendraShinde@synergetics-india.com>
2026-01-07 17:30:13 +0530

    Added a delay in customer service

--- customer-service/src/main/java/com/mahendra/rest/CustomerResource.java
+++ customer-service/src/main/java/com/mahendra/rest/CustomerResource.java
@@ -15,10 +15,22 @@ public class CustomerResource {
 	@GetMapping(value="/{ID}", produces="application/json")
 	public ResponseEntity<Customer> findCustomer(@PathVariable("ID") int id )
 	{
+		delayProcessing(2);
 		if(id >=1 && id <=5) {
+			delayProcessing(5);
 			Customer cust = new Customer("C"+id,"User "+id , "Rao", "user-"+id+"@xyz.com");
 			return ResponseEntity.ok(cust);
 		}else
 			return ResponseEntity.status(404).build();
+	}
+	
+	
+	private void delayProcessing(int seconds) {
+		try {
+		Thread.sleep(seconds*1000);
+		}catch(InterruptedException ex) {
+			System.out.println("Exception while delayed");
+		}
+		
 	}
 }
