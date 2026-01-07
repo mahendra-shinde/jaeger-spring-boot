9e77d01369ca69e0eabe76b1d569f6aa68ef5181
Mahendra shinde <MahendraShinde@synergetics-india.com>
2026-01-07 18:23:29 +0530

    Refactor LoanResource to enhance delay processing logic

--- loan-service/src/main/java/com/mahendra/rest/LoanResource.java
+++ loan-service/src/main/java/com/mahendra/rest/LoanResource.java
@@ -26,14 +26,25 @@ public class LoanResource {
 
 	@GetMapping(value="{ACCNO}", produces = "application/json")
 	public Loan findLoan(@PathVariable("ACCNO") String accNo) {
-		
+		delayProcessing(2);
 		//Customer cust = client.getForObject(customerEndpoint +"/api/customer/1", Customer.class);
 		ResponseEntity<Customer> resp = client.getForEntity(customerEndpoint +"/api/customer/1", Customer.class);
 		Customer cust = null;
 		if(resp.getStatusCode() == HttpStatus.OK) {
+			delayProcessing(2);
 			cust = resp.getBody();
 		}
+		delayProcessing(1);
 		Loan loan = new Loan(accNo, 8030000.00, 9.55, LocalDate.of(2021,2, 12), 36540.00, cust);
 		return loan;
+	}
+	
+	private void delayProcessing(int seconds) {
+		try {
+		Thread.sleep(seconds*1000);
+		}catch(InterruptedException ex) {
+			System.out.println("Exception while delayed");
+		}
+		
 	}
 }
